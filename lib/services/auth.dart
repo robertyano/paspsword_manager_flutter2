1fe4ceb44e180ac10dd9b7f398944ec6a2ee408f
robertyano <robertyano@gmail.com>
2023-06-18 16:30:11 -0400

    Setting up initial screens, models, and wrappers (part 3) - Added Flutter SpinKit to project (updated pubspec.yaml) - Added Auth.dart and Loading.dart files

---
+++ lib/services/auth.dart
@@ -0,0 +1,67 @@
+import 'package:paspsword_manager_flutter2/models/theuser.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user object based on FirebaseUser
+  theUser? _userFromFirebaseUser(User? user) {
+    return user != null ? theUser(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<theUser?> get user {
+    return _auth.authStateChanges()
+        .map(_userFromFirebaseUser);
+  }
+
+
+  // sign in anon
+  Future signInAnon() async {
+    await Firebase.initializeApp();
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email & password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // register with email & password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+}
