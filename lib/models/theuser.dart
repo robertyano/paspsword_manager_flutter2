71384f4cad6b73af3b3ac9a2bbc57d24d497aeba
robertyano <robertyano@gmail.com>
2023-06-18 16:43:21 -0400

    Setting up initial screens, models, and wrappers (part 4) - Connecting Firebase - Updating theuser.dart

--- lib/models/theuser.dart
+++ lib/models/theuser.dart
@@ -1,32 +1,7 @@
-import 'package:paspsword_manager_flutter2/screens/wrapper.dart';
-import 'package:paspsword_manager_flutter2/services/auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'models/theuser.dart';
-import 'package:flutter/material.dart';
+class theUser {
 
-Future <void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  final String? uid;
+
+  theUser({ this.uid});
+
 }
-
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return StreamProvider<theUser?>.value(
-      initialData: null,
-      value: AuthService().user,
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Wrapper(),
-      ),
-    );
-  }
-}
-
