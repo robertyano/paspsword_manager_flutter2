4b3aab71cdabf1f02e16be28b92695998bb797b0
robertyano <robertyano@gmail.com>
2023-08-26 18:49:21 -0400

    test commit2

--- lib/models/theuser.dart
+++ lib/models/theuser.dart
@@ -3,7 +3,8 @@ import 'package:paspsword_manager_flutter2/models/account.dart';
 class theUser {
 
   final String? uid;
+  final String? test;
 
-  theUser({ this.uid});
+  theUser({ this.uid, this.test});
 
 }
