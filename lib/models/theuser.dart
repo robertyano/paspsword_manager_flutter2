a5de9d2d13027f2266250cfdcec6d739f4198a59
robertyano <robertyano@gmail.com>
2023-08-26 18:52:20 -0400

    remove test from theuser

--- lib/models/theuser.dart
+++ lib/models/theuser.dart
@@ -3,8 +3,7 @@ import 'package:paspsword_manager_flutter2/models/account.dart';
 class theUser {
 
   final String? uid;
-  final String? test;
 
-  theUser({ this.uid, this.test});
+  theUser({ this.uid});
 
 }
